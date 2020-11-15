c2eca161bfb88fe2f433abcc30b997ea8304fd7f
scouts107 <74025070+scouts107@users.noreply.github.com>
2020-11-15 13:48:45 -0500

    Update CareTaker.java

--- SyllabusScheduler/src/CareTaker.java
+++ SyllabusScheduler/src/CareTaker.java
@@ -2,13 +2,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CareTaker {
-   private List<Memento> mementoList = new ArrayList<Memento>();
+   private ArrayList<Memento> mementoList = new ArrayList<Memento>();
 
-   public void add(Memento state){
+   public void addMemento(Memento state){
       mementoList.add(state);
    }
 
-   public Memento get(int index){
-      return mementoList.get(index);
+   public Memento getMemento(){
+      return mementoList.get(0);
    }
 }
